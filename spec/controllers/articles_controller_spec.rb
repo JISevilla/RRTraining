e29dc2fd69af90ab40d73c45bd9123314bbefe24
Julian <julianisevilla@gmail.com>
2015-05-15 13:32:10 -0300

    Implemented Mailer

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 require 'rails_helper'
 describe ArticlesController do
   let(:user) { create(:user) }
@@ -15,3 +18,42 @@ describe ArticlesController do
     end
   end
 end
+=======
+describe 'GET #article' do
+  context 'when params[:id] == id' do
+	  it 'return the correct article' do
+	    @article.id.should eql id
+	  end
+	end
+end
+>>>>>>> spec files
+=======
+require "rails_helper" 
+=======
+require "rails_helper"
+<<<<<<< HEAD
+
+>>>>>>> spec models
+=======
+>>>>>>> first tests done
+describe ArticlesController do
+  let(:user) { create(:user) }
+
+  describe 'POST #create' do
+    context 'When creating an article without error' do
+      before(:each) do
+        sign_in(user)
+        user.reload
+      end
+
+      it 'Has one more article' do
+        expect { post :create, article: { title: '12345', body: 'body' } }.to change{Article.count}.by(1)
+      end
+    end
+  end
+<<<<<<< HEAD
+end
+>>>>>>> articles_controller changed
+=======
+end
+>>>>>>> spec models
--- spec/models/article_model_spec.rb
+++ spec/models/article_model_spec.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'rails_helper'
 describe Article do
   describe '#create' do
@@ -17,5 +18,27 @@ describe Article do
         expect(article.valid?).to be false
       end
     end
+=======
+require "rails_helper"
+
+describe Article do
+	describe '#create' do
+    let(:article) {create(:article) }
+      context 'article without error' do
+        it 'Create article' do
+          expect(article.valid?).to be true
+        end
+      end
+      context 'article with error' do
+        it 'article title null' do
+          article.title = nil
+          expect(article.valid?).to be false
+        end
+        it 'article title too short' do
+          article.title = "nil"
+          expect(article.valid?).to be false
+        end
+      end
+>>>>>>> spec models
   end
 end
