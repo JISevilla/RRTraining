14fce8ed2e9185865ba1135f49c161ab91fbd264
Julian <julianisevilla@gmail.com>
2015-05-18 09:11:20 -0300

    articles_controller changed

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,7 +1,22 @@
-describe 'GET #article' do
-  context 'when params[:id] == id' do
-	  it 'return the correct article' do
-	    @article.id.should eql id
-	  end
-	end
+require "rails_helper" 
+describe ArticlesController do
+  describe 'GET #index' do
+    context 'when user is logged in' do
+      with :user
+      before do
+        sign_in user
+        get :index
+      end
+      it { is_expected.to redirect_to new_session_path }
+      it { is_expected.to set_the_flash(:warning).to('Please log in.') }
+    end
+    context 'when user is logged out' do
+      before do
+        get :index
+      end
+      it { is_expected.to redirect_to new_session_path }
+      it { is_expected.to set_the_flash(:warning).to('Please log in.') }
+      it { is_expected.to set_session(:return_to).to(posts_path) }
+    end
+  end
 end
