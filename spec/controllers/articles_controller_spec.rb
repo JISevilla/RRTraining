8ab78fc1f890793cdd6393d8c03ebcdf4b50786b
Julian <julianisevilla@gmail.com>
2015-05-18 09:11:20 -0300

    articles_controller changed

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 require 'rails_helper'
 describe ArticlesController do
   let(:user) { create(:user) }
@@ -25,3 +26,27 @@ describe 'GET #article' do
 	end
 end
 >>>>>>> spec files
+=======
+require "rails_helper" 
+describe ArticlesController do
+  describe 'GET #index' do
+    context 'when user is logged in' do
+      with :user
+      before do
+        sign_in user
+        get :index
+      end
+      it { is_expected.to redirect_to new_session_path }
+      it { is_expected.to set_the_flash(:warning).to('Please log in.') }
+    end
+    context 'when user is logged out' do
+      before do
+        get :index
+      end
+      it { is_expected.to redirect_to new_session_path }
+      it { is_expected.to set_the_flash(:warning).to('Please log in.') }
+      it { is_expected.to set_session(:return_to).to(posts_path) }
+    end
+  end
+end
+>>>>>>> articles_controller changed
