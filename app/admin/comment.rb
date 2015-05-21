851f1ac6b72d1f1e9e9a4761b849e486ed02ee7a
Julian <julianisevilla@gmail.com>
2015-05-21 14:35:58 -0300

    admin done

--- app/admin/comment.rb
+++ app/admin/comment.rb
@@ -1,6 +1,6 @@
-ActiveAdmin.register Article do
+ActiveAdmin.register Comment , as: 'User Comments' do
 
-  permit_params :text, :title
+  permit_params :commenter, :body
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
